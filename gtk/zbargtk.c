0e96b9eed5c0d52fbcd443fc4cef54880f9fe144
Mauro Carvalho Chehab <mchehab+samsung@kernel.org>
2018-08-08 11:40:25 -0300

    zbar: use g_thread_new() instead of g_tread_create()
    gtk/zbargtk.c: In function ‘zbar_gtk_init’:
    gtk/zbargtk.c:630:5: warning: ‘g_thread_create’ is deprecated: Use 'g_thread_new' instead [-Wdeprecated-declarations]
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab+samsung@kernel.org>

--- gtk/zbargtk.c
+++ gtk/zbargtk.c
@@ -627,8 +627,7 @@ static void zbar_gtk_init (ZBarGtk *self)
 
     /* spawn a thread to handle decoding and video */
     zbar->queue = g_async_queue_new();
-    zbar->thread = g_thread_create(zbar_gtk_processing_thread, self,
-                                    FALSE, NULL);
+    zbar->thread = g_thread_new(NULL, zbar_gtk_processing_thread, self);
     g_assert(zbar->thread);
 }
 
