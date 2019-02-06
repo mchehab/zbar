21a1890a6265a358f818e68ef00c6759ccfbe202
Mauro Carvalho Chehab <mchehab+samsung@kernel.org>
2019-02-06 16:09:29 -0200

    test_dbus: add copyright notes
    Let it clear that this file is under LGPL 2.1, as the
    remaining Zbar code.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab+samsung@kernel.org>

--- test/test_dbus.c
+++ test/test_dbus.c
@@ -1,3 +1,19 @@
+/*------------------------------------------------------------------------
+ *  Copyright 2019 (c) Mauro Carvalho Chehab <mchehab+samsung@kernel.org>
+ *
+ *  This file is part of the ZBar Bar Code Reader.
+ *
+ *  The ZBar Bar Code Reader is free software; you can redistribute it
+ *  and/or modify it under the terms of the GNU Lesser Public License as
+ *  published by the Free Software Foundation; either version 2.1 of
+ *  the License, or (at your option) any later version.
+ *
+ *  The ZBar Bar Code Reader is distributed in the hope that it will be
+ *  useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ *  of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser Public License for more details.
+ *------------------------------------------------------------------------*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
