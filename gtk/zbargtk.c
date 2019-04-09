d61b2555076c29f2b8c72e9f1582efaff0e098e9
Руслан Ижбулатов <lrn1986@gmail.com>
2019-04-09 09:38:41 -0300

    Make use of glib thread names
    This is optional, but why not use it?
    
    Signed-off-by: Руслан Ижбулатов <lrn1986@gmail.com>

--- gtk/zbargtk.c
+++ gtk/zbargtk.c
@@ -637,7 +637,7 @@ static void zbar_gtk_init (ZBarGtk *self)
 
     /* spawn a thread to handle decoding and video */
     zbar->queue = g_async_queue_new();
-    zbar->thread = g_thread_new(NULL, zbar_gtk_processing_thread, self);
+    zbar->thread = g_thread_new("zbar gtk processing thread", zbar_gtk_processing_thread, self);
     g_assert(zbar->thread);
 }
 
