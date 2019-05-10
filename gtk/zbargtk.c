e5b3b17a4ab9529e228dafe7fd2f674460977d8d
Mauro Carvalho Chehab <mchehab+samsung@kernel.org>
2019-05-10 20:52:55 -0300

    zbargtk: cleanup the last two deprecated warnings
    The two deprecated warnings that were seeking for a fix were:
    
    1) disable double-buffered mode. This should improve performance,
    but it is otherwise useless. So, we can live without that.
    
    2) set the custom widget with a background. From other
    similar code, it seems that this is required only up to
    Gtk 3.18, so we can simply get rid of it.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab+samsung@kernel.org>

--- gtk/zbargtk.c
+++ gtk/zbargtk.c
@@ -343,9 +343,13 @@ static void zbar_gtk_realize (GtkWidget *widget)
         return;
     ZBarGtkPrivate *zbar = ZBAR_GTK_PRIVATE(self->_private);
 
-    gtk_widget_set_double_buffered(widget, FALSE);
     gtk_widget_set_realized(widget, TRUE);
 
+#if GTK_MAJOR_VERSION >= 3 && GTK_MINOR_VERSION >= 14
+    // FIXME: this is deprecated after 3.14 - no idea what replaces it
+#else
+    gtk_widget_set_double_buffered(widget, FALSE);
+#endif
     GdkWindowAttr attributes;
     GtkAllocation allocation;
 
@@ -364,9 +368,9 @@ static void zbar_gtk_realize (GtkWidget *widget)
                                        GDK_WA_X | GDK_WA_Y);
     gtk_widget_set_window(widget, window);
     gdk_window_set_user_data(window, widget);
-#if GTK_MAJOR_VERSION >= 3
+#if GTK_MAJOR_VERSION >= 3 && GTK_MINOR_VERSION >= 18
     gdk_window_set_background_pattern(window, NULL);
-#else
+#elif GTK_MAJOR_VERSION < 3
     gdk_window_set_back_pixmap(window, NULL, TRUE);
 #endif
 
