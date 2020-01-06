c7f3f1698083cbc84f9e65bceb778aad6b96c1f2
Animal451 <ben.johnstone@gmail.com>
2020-01-06 13:45:44 +0000

    Update ZBarHelpController.m to use WKWebView
    As per Issue #17, Apple have deprecated UIWebView, so these changes are to get ZBar iPhone to user the WKWebView instead.

--- iphone/ZBarHelpController.m
+++ iphone/ZBarHelpController.m
@@ -84,11 +84,11 @@
     view.autoresizingMask = (UIViewAutoresizingFlexibleWidth |
                              UIViewAutoresizingFlexibleHeight);
 
-    webView = [[UIWebView alloc]
+    webView = [[WKWebView alloc]
                   initWithFrame: CGRectMake(0, 0,
                                             bounds.size.width,
                                             bounds.size.height - 44)];
-    webView.delegate = self;
+    //webView.delegate = self;
     webView.backgroundColor = [UIColor colorWithWhite: .125f
                                        alpha: 1];
     webView.autoresizingMask = (UIViewAutoresizingFlexibleWidth |
@@ -154,14 +154,14 @@
     assert(webView);
     if(webView.loading)
         webView.hidden = YES;
-    webView.delegate = self;
+    //webView.delegate = self;
     [super viewWillAppear: animated];
 }
 
 - (void) viewWillDisappear: (BOOL) animated
 {
     [webView stopLoading];
-    webView.delegate = nil;
+    //webView.delegate = nil;
     [super viewWillDisappear: animated];
 }
 
@@ -210,7 +210,7 @@
         [self dismissModalViewControllerAnimated: YES];
 }
 
-- (void) webViewDidFinishLoad: (UIWebView*) view
+- (void) webViewDidFinishLoad: (WKWebView*) view
 {
     if(view.hidden) {
         [view stringByEvaluatingJavaScriptFromString:
@@ -234,9 +234,9 @@
     }
 }
 
-- (BOOL)             webView: (UIWebView*) view
+- (BOOL)             webView: (WKWebView*) view
   shouldStartLoadWithRequest: (NSURLRequest*) req
-              navigationType: (UIWebViewNavigationType) nav
+              navigationType: (WKNavigationType) nav
 {
     NSURL *url = [req URL];
     if([url isFileURL])
