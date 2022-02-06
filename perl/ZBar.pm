93564a58159c6d2736453401545b377993e565a1
Timothy Legge <timlegge@gmail.com>
2022-02-06 12:33:35 -0400

    Add provides and min-perl version to meta files

--- perl/ZBar.pm
+++ perl/ZBar.pm
@@ -32,7 +32,7 @@ our @ISA = qw(Exporter);
 our @EXPORT_OK = qw(SPACE BAR
                     version increase_verbosity set_verbosity);
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 require XSLoader;
 XSLoader::load('Barcode::ZBar', $VERSION);
